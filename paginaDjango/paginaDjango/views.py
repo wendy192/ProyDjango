8525a8fb532837bff126b95b3c8fc33e78f34521
Wendy <88361194+wendy192@users.noreply.github.com>
2022-06-16 18:12:55 -0500

    .

--- paginaDjango/paginaDjango/views.py
+++
@@ -1,7 +0,0 @@
-import datetime
-from django.shortcuts import render 
-
-
-def cursoc(request):
-    fechaCurso=datetime.datetime.now()
-    return render(request,"CursoC.html",{'dameFecha':fechaCurso})
