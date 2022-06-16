8475b0f09f88501a6bc92df242b8998a2985b603
Wendy <88361194+wendy192@users.noreply.github.com>
2022-06-16 17:15:24 -0500

    .

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from Proyecto1.views import saludo,despedida,damefecha,calculaEdad,cursoc
 
 urlpatterns = [
